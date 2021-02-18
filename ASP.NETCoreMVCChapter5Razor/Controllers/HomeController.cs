4d28021768fed9cf1b11024a655f336d77a3d360
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-02-18 22:02:21 +0700

    Создание Empty-проекта и подготовка к  работе.

---
+++ ASP.NETCoreMVCChapter5Razor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NETCoreMVCChapter5Razor.Models;
+
+namespace ASP.NETCoreMVCChapter5Razor.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductId = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+            return View(myProduct);
+        }
+    }
+}
